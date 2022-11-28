10b064a5b382181e2a93167e765087b708bcb314
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-28 11:03:27 +0100

    Update PierreLCSTR.py

--- PierreLCSTR.py
+++ PierreLCSTR.py
@@ -14,7 +14,7 @@ st.write("PS : c'est un super formateur de la Wild Code School")
 df = pd.read_csv("velib.csv")
 
 # Quasiment tous les éléments streamlit peuvent être affichés dans la "sidebar"
-st.sidebar.image('pierre_mur.jpg', width=300)
+st.sidebar.image('pierre_mur[1].jpg', width=300)
 st.sidebar.title("Cette Sidebar est inutile mais rien ne vaut une photo de Pierre Mur pour égayer votre journée de Novembre !")
 st.sidebar.image('pierre_mur.jpg', width=300)
 st.sidebar.write("Pierre Mur , formateur EMERITUS de la Wild CODE SCHOOL. Tous droits à l'image réservés tout ça !")
